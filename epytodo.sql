1d5618fb7efa72ba8359e1b2a951f58df66a666e
Mohamed Tajdirt <mohamed.tajdirt@epitech.eu>
2025-05-13 15:24:02 +0200

    fix(epytodo): fix not null on the db

--- epytodo.sql
+++ epytodo.sql
@@ -7,16 +7,16 @@ CREATE TABLE user (
     password VARCHAR(255) NOT NULL,
     name VARCHAR(255) NOT NULL,
     firstname VARCHAR(255) NOT NULL,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
 CREATE TABLE todo (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     description TEXT NOT NULL,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
     due_time DATETIME NOT NULL,
     status ENUM("not started", "todo", "in progress", "done") DEFAULT "not started",
-    user_id INT,
+    user_id INT NOT NULL,
     FOREIGN KEY (user_id) REFERENCES user(id)
 );
