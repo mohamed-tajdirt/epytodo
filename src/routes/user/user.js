3f79901cb8ef55f4eab7143e994cf09eea2c1a09
Tom Chatelain <tom.chatelain@epitech.eu>
2025-05-15 20:01:35 +0200

    fit( change getuser to avoid error)

--- src/routes/user/user.js
+++ src/routes/user/user.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { getCurrentUser, getUserTodos, updateUser, deleteUser, getUserById, getUserByEmail} = require('./user.query');
+const { getCurrentUser, getUserTodos, updateUser, deleteUser, getUserinfo} = require('./user.query');
 const authToken = require('../../middleware/auth');
 const badParameter = require('../../middleware/badparameter');
 
 router.get('/user', authToken, getCurrentUser);
 router.get('/user/todos', authToken, getUserTodos);
-router.get('/users/:id', authToken, getUserById);
+router.get('/users/:id', authToken, getUserinfo);
 router.put('/users/:id', authToken, updateUser); // ?
 router.delete('/users/:id', authToken, deleteUser); // ?
 
--- src/routes/user/user.query.js
+++ src/routes/user/user.query.js
@@ -28,37 +28,39 @@ const getUserTodos = (req, res) => {
     });
 };
 
-const getUserById = (req, res) => {
-    const user_id = req.params.id
+const getUserinfo = (req, res) => {
+    const user_id = req.params.id;
+
     if (!user_id)
-        return res.status(400).json({msg: "Bad parameter"});
+        return res.status(400).json({ msg: "Bad parameter" });
+
     db.query("SELECT * FROM user WHERE id = ?", [user_id], (err, results) => {
         if (err)
             return res.status(500).json({ msg: 'Internal server error' });
-        if (results.length === 0)
-            return res.status(404).json({msg: "User not found."});
+        if (results.length === 0) {
+            return db.query("SELECT * FROM user WHERE email = ?", [user_id], (err, result) => {
+                if (err)
+                    return res.status(500).json({ msg: 'Internal server error' });
+                if (result.length === 0)
+                    return res.status(404).json({ msg: "User not found." });
+                return res.status(200).json({
+                    id: result[0].id,
+                    email: result[0].email,
+                    password: result[0].password,
+                    created_at: result[0].created_at,
+                    firstname: result[0].firstname,
+                    name: result[0].name
+                });
+            });
+        }
         return res.status(200).json({
-                id: results[0].id,
-                email: results[0].email,
-                password: results[0].password,
-                created_at: results[0].created_at,
-                firstname: results[0].firstname,
-                name: results[0].name
+            id: results[0].id,
+            email: results[0].email,
+            password: results[0].password,
+            created_at: results[0].created_at,
+            firstname: results[0].firstname,
+            name: results[0].name
         });
-    });
-};
-
-const getUserByEmail = (req, res) => {
-    const email = req.params.email
-
-    if (!email)
-        return res.status(400).json({msg: "Bad parameter"});
-    db.query("SELECT * FROM user WHERE email = ?", [email], (err, results) => {
-        if (err)
-            return res.status(500).json({ msg: 'Internal server error' });
-        if (results.length === 0)
-            return res.status(404).json({msg: "User not found."});
-        return res.status(200).json(results[0]);
     });
 };
 
@@ -105,4 +107,4 @@ const deleteUser = (req, res) => {
     })
 };
 
-module.exports = { getCurrentUser, getUserTodos, updateUser, deleteUser, getUserById, getUserByEmail };
+module.exports = { getCurrentUser, getUserTodos, updateUser, deleteUser, getUserinfo };
